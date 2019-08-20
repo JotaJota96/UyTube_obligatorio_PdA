2a1b2006655a593644f444481bc1b98c60100610
JotaJota96 <jjap96@gmail.com>
2019-08-20 02:48:12 -0300

    DataType de Usuario hecho

--- UyTube_aplicacion/src/Logica/main.java
+++ UyTube_aplicacion/src/Logica/main.java
@@ -1,9 +1,16 @@
 package Logica;
 
+
+
+import java.util.Date;
+
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        Date d = new Date(2019-1900, 8 -1, 20);
         
-        // escriba aqui sus pruebas
+        System.out.println(d);
+        
     }
 }
