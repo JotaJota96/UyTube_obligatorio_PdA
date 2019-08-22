1b6b47179e3c8ba8ea7397caf0ea2521797bdfd3
Alfa-J <alfa-J@gmail.com>
2019-08-22 02:11:26 -0300

    agrege throw a comentario y valoracion

--- UyTube_aplicacion/src/Logica/Clases/Comentario.java
+++ UyTube_aplicacion/src/Logica/Clases/Comentario.java
@@ -21,6 +21,12 @@ public class Comentario {
     }
 
     public Comentario(int id, Date fecha, String texto, int nivelSubComentario, Usuario usr) {
+        if(usr==null){
+            throw new RuntimeException("Usuario No puede ser null");
+        }
+        if(fecha==null){
+            throw new RuntimeException("Date No puede ser null");
+        }
         this.id = id;
         this.fecha = fecha;
         this.texto = texto;
@@ -46,6 +52,9 @@ public class Comentario {
     }
 
     public void setFecha(Date fecha) {
+        if(fecha==null){
+            throw new RuntimeException("Fecha No puede ser null");
+        }
         this.fecha = fecha;
     }
 
@@ -69,16 +78,18 @@ public class Comentario {
         return usr;
     }
 
-    public void setUsr(Usuario usr) {
-        this.usr = usr;
-    }
-
     @Override
     public String toString() {
         return "Comentario{" + "id=" + id + ", fecha=" + fecha + ", texto=" + texto + ", nivelSubComentario=" + nivelSubComentario + '}';
     }
 
     public boolean agregarSubComentario(int idComPadre, DtComentario dtC, Usuario usr) {
+        if(dtC==null){
+            throw new RuntimeException("DtComentario No puede ser null");
+        }
+        if(usr==null){
+            throw new RuntimeException("Usuario No puede ser null");
+        }
         if (this.id == idComPadre) {
             Comentario com = new Comentario(Comentario.getNuevoID(),
                     dtC.getFecha(),
