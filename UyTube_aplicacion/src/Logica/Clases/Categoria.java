7e93380492b081a2538d5195c2a31d025c96f4d1
JotaJota96 <jjap96@gmail.com>
2019-08-26 13:26:09 -0300

    Clase Categoria cubierta 100%

--- UyTube_aplicacion/src/Logica/Clases/Categoria.java
+++ UyTube_aplicacion/src/Logica/Clases/Categoria.java
@@ -4,6 +4,9 @@ public class Categoria {
     private String nombre;
 
     public Categoria(String nombre) {
+        if (nombre.equals("")){
+            throw new RuntimeException("El nombre de la categoria no puede ser vacio");
+        }
         this.nombre = nombre;
     }
 
