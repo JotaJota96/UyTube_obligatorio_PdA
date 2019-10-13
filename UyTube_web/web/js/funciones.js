fd447bcf34bfa77809d73e721b2102cdc72fa933
Julio Arrieta <julioarrieta23@gmail.com>
2019-10-13 16:44:06 -0300

    Se refactoriza conetnido-index,header,footer,widget,menu, para poder usar de forma más general

--- UyTube_web/web/js/funciones.js
+++ UyTube_web/web/js/funciones.js
@@ -1,8 +1,18 @@
+
 // Muestra u oculta el menu y ajusta su posicion en altura
 $(".icon-menu1").on("click", function () {
-    var posicionMenu = parseInt($(".header").css("height"));
-    $(".menu").css("top", posicionMenu);
     $(".menu").toggleClass('abrir');
+    // screenHeight = $(window).height();
+    // altoHeader = parseFloat($(".header").css('height'));
+    // altoMenu = parseFloat($(".menu").css('height'));
+    // if(screenHeight < (altoHeader+altoMenu) ){
+    //     console.log('el alto de menu es superior a la pantalla');
+    //     $(".menu").css('height',(screenHeight-altoHeader));
+    // }else{
+    //    // $(".menu").css('height:350px');
+    //     console.log('el alto de menu es: '+ $(".menu").css('height:350px'));
+    // }
+    // console.log(" alto de pantalla: " + screenHeight+", alto del menu: "+altoMenu);
 });
 
 /*
@@ -14,23 +24,21 @@ $(".icon-menu1").on("click", function(){
 });
 */
 
-screenWidth = $(window).width();
-screenHeight = $(window).height();
 
-setInterval(function () {
-    if ($(window).width() !== screenWidth || $(window).height() !== screenHeight) {
-        screenHeight = 0;
-        altoMenu = 0;
-        screenHeight = $(window).height();
-        altoHeader = parseFloat($(".header").css('height'));
-        altoMenu = parseFloat($(".menu").css('height'));
-        if(screenHeight < (altoHeader+altoMenu) ){
-            console.log('el alto de menu es superior a la pantalla');
-            $(".menu").css('height',(screenHeight-altoHeader));
-        }else{
-            $(".menu").css('height','350px');
-            console.log('el alto de menu es: 350px');
-        }
-        console.log('ancho: ' + screenWidth + ", alto: " + screenHeight);
-    }
-}, 50);
+// setInterval(function () {
+//     if ($(window).width() !== screenWidth || $(window).height() !== screenHeight) {
+//         screenHeight = 0;
+//         altoMenu = 0;
+//         screenHeight = $(window).height();
+//         altoHeader = parseFloat($(".header").css('height'));
+//         altoMenu = parseFloat($(".menu").css('height'));
+//         if(screenHeight < (altoHeader+altoMenu) ){
+//             console.log('el alto de menu es superior a la pantalla');
+//             $(".menu").css('height',(screenHeight-altoHeader));
+//         }else{
+//             $(".menu").css('height','350px');
+//             console.log('el alto de menu es: 350px');
+//         }
+//         console.log('ancho: ' + screenWidth + ", alto: " + screenHeight);
+//     }
+// }, 50);
