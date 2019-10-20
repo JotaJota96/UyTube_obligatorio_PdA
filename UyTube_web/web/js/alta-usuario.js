49538cb9790f61d89486a0d862245266a693eaa1
Julio Arrieta <julioarrieta23@gmail.com>
2019-10-20 12:47:27 -0300

    modificando las validaciones de alta usuario, antes del commit con pre_master

--- UyTube_web/web/js/alta-usuario.js
+++ UyTube_web/web/js/alta-usuario.js
@@ -13,6 +13,9 @@ $("#input_Nickname").keyup(function(){
         }, function(respuesta, status){
         //alert("Data: " + respuesta + "\nStatus: " + status);
         $("#msjNickname").text(respuesta);
+        if(respuesta == "Este nickname no está disponible"){
+          $("#msjNickname").css("color","red");
+        }
     });
 }); 
 
@@ -32,4 +35,21 @@ $("#email").keyup(function(){
         $("#msjEmail").text(respuesta);
     });
 }); 
+
+var check = function () {
+  if (document.getElementById('input_Contrasenia').value ==document.getElementById('input_Repetir_contraseña').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'Correcto';
+      document.getElementById("btn_Registrarme").disabled = false;
+      if (document.getElementById('input_Contrasenia').value == ""){
+          document.getElementById('message').style.color = 'red';
+          document.getElementById('message').innerHTML = 'Ingrese la contraseña';
+          document.getElementById("btn_Registrarme").disabled = true;
+      }
+  } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'No son iguales';
+      document.getElementById("btn_Registrarme").disabled = true;
+  }
+}
 
